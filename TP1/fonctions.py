91bec220f09ca219d6a093fad113e89a56722c10
Kylan <kylan.delpouve@univ-rouen.fr>
2026-09-16 13:58:11 +0200

    mise a jour fichiers fonctions et test

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,8 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int: # vérifie si a et b sont des entiers
         raise TypeError("Only integers are allowed") # si a ou b n'est pas un entier, lance une erreur
+
+    if a == 0 and b < 0:
+        raise ZeroDivisionError("0 ne peut pas etre eleve a une puissance negative")
+
     return a ** b
--- TP1/test.py
+++ TP1/test.py
@@ -6,8 +6,15 @@ def test_1():
     assert f.puissance(2,2) == 4
 
 def test_2():
-	assert f.puissance(-1,3) == -1
-	assert f.puissance(-1,2) == 1
-	assert f.puissance(-1,-1) == -1
-	assert f.puissance(-1,-2) == 1
-	assert f.puissance(-2,-1) == -0.5
+    assert f.puissance(-1,3) == -1
+    assert f.puissance(-1,2) == 1
+    assert f.puissance(-1,-1) == -1
+    assert f.puissance(-1,-2) == 1
+    assert f.puissance(-2,-1) == -0.5
+
+def test_3():
+    assert f.puissance(0,2) == 0
+    assert f.puissance(0,5) == 0
+
+    with pytest.raises(ZeroDivisionError):
+        f.puissance(0,-1)
